1a27a2057aadfd29244d4b44a97e69f8280dd792
mlodyFrontendowiec <pawel.kruszelnicki@onet.pl>
2021-10-14 20:33:14 +0200

    Initial project setup

---
+++ src/components/Board.vue
@@ -0,0 +1,9 @@
+<template> </template>
+
+<script>
+export default {
+  name: "Board",
+};
+</script>
+
+<style></style>
---
+++ src/components/Cell.vue
@@ -0,0 +1,9 @@
+<template> </template>
+
+<script>
+export default {
+  name: "Cell",
+};
+</script>
+
+<style></style>
