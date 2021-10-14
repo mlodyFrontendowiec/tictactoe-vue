b4ff3e14ca310c0ae60aafb7a7cb003924c9646d
mlodyFrontendowiec <pawel.kruszelnicki@onet.pl>
2021-10-14 22:59:06 +0200

    Finished

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -17,20 +17,50 @@ export default {
   data: () => ({
     arrOfLetters: ["", "", "", "", "", "", "", "", ""],
     actualChar: "O",
+    step: 0,
   }),
   methods: {
     setChar(id) {
       if (this.arrOfLetters[id] !== "") return;
       this.arrOfLetters[id] = this.actualChar;
-      this.changeChar();
+      setTimeout(() => {
+        this.checkWhoWin();
+      });
+      this.step++;
     },
     changeChar() {
       if (this.actualChar === "O") {
         this.actualChar = "X";
       } else this.actualChar = "O";
     },
+    checkWhoWin() {
+      if (
+        (this.arrOfLetters[0] === this.actualChar &&
+          this.arrOfLetters[1] === this.actualChar &&
+          this.arrOfLetters[2] === this.actualChar) ||
+        (this.arrOfLetters[3] === this.actualChar &&
+          this.arrOfLetters[4] === this.actualChar &&
+          this.arrOfLetters[5] === this.actualChar) ||
+        (this.arrOfLetters[6] === this.actualChar &&
+          this.arrOfLetters[7] === this.actualChar &&
+          this.arrOfLetters[8] === this.actualChar) ||
+        (this.arrOfLetters[0] === this.actualChar &&
+          this.arrOfLetters[4] === this.actualChar &&
+          this.arrOfLetters[8] === this.actualChar) ||
+        (this.arrOfLetters[2] === this.actualChar &&
+          this.arrOfLetters[4] === this.actualChar &&
+          this.arrOfLetters[6] === this.actualChar)
+      ) {
+        alert("The winner is " + this.actualChar);
+        window.location.reload();
+      } else if (this.step === 9) {
+        alert("DRAW");
+        window.location.reload();
+      } else {
+        this.changeChar();
+      }
+    },
   },
-  computed: {},
 };
 </script>
 <style scoped>
